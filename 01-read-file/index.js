a459f1cb11cc7e0a286e4dabe3a047fe398ffb20
Kristina Naydenova <kristina.shpakovskaya@gmail.com>
2023-05-06 18:40:08 +0200

    fix: encoding

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,13 @@
+const path = require('path');
 const fs = require('fs');
 
-const rs = fs.createReadStream('text.txt', 'utf-8');
+const filePath = path.join(__dirname, 'text.txt');
+const rs = fs.createReadStream(filePath, { encoding: 'utf-8' });
+
 rs.on('data', (chunk) => {
   console.log(chunk);
 });
+
 rs.on('error', (err) => {
   console.error(err);
 });

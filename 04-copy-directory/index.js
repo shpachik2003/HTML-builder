f5435b4dd2ac13566ff787887c5e07f55215edf7
Kristina Naydenova <kristina.shpakovskaya@gmail.com>
2023-05-06 20:40:14 +0200

    init: solve 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
 
+const sourceDir = path.join(__dirname, 'files');
+const targetDir = path.join(__dirname, 'files-copy');
+
+const copyDir = (src, dest) => {
+  if (!fs.existsSync(dest)) {
+    fs.mkdirSync(dest);
+  }
+  
+  const files = fs.readdirSync(src);
+  
+  files.forEach(file => {
+    const srcPath = path.join(src, file);
+    const destPath = path.join(dest, file);
+
+    if (fs.statSync(srcPath).isDirectory()) {
+      copyDir(srcPath, destPath);
+    } else {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  });
+};
+
+copyDir(sourceDir, targetDir);
+console.log('Содержимое папки скопировано');
